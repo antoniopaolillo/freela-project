d001708b1058ac0f764cdf1df0c0b10c3201e4a4
antoniopaolillo <antonio_francisco_p@live.com>
2020-10-05 17:21:20 -0300

    add paralax basic

--- linkin-firm/src/pages/Home.jsx
+++ linkin-firm/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Header from '../components/Header'
 import HomeInfo from '../components/HomeInfo';
 import HomeFooter from '../components/HomeFooter';
 import HomeCarousel from '../components/HomeCarousel';
+import Paralax from '../components/Paralax';
 
 function Home() {
   return (
     <div>
       <Header />
+      <Paralax />
       <HomeInfo />
       <HomeCarousel />
       <HomeFooter />
