5ecae80d3e4f1de691867206c4c8d3e5b2bbc541
antoniopaolillo <antonio_francisco_p@live.com>
2020-10-05 10:34:19 -0300

    fix mistake in contact style

--- linkin-firm/src/pages/Home.jsx
+++ linkin-firm/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../components/Header'
+import HomeInfo from '../components/HomeInfo';
+import HomeFooter from '../components/HomeFooter';
 
 function Home() {
   return (
